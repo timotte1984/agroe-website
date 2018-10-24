2d06f5df715191df1e83b074e9137a657b4f80b5
Julien Mottet <mottet.julien123@gmail.com>
2018-10-24 22:08:57 +0200

    che pu

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'ecosystem', to: 'pages#ecosystem'
   get 'temp_page', to: 'pages#temp_page'
+  get 'temp_page2', to: 'pages#temp_page2'
   resources 'users', only: [:new, :edit, :show, :index]
   resources 'associations', only: [:new, :create, :show, :index]
 
