3df4cc715c05f1d32b0767e5edbc19a6d78a02c9
Julien Mottet <mottet.julien123@gmail.com>
2019-05-10 22:36:43 +0200

    fixing constructor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  before_filter :verify_email
 
   def home
-  end
-
-  private
-
-  def verify_email
-    redirect_to(root_path) unless current_user.email.include?('@xyz.com')
   end
 
 end
