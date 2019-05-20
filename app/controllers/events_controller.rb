3cee1819ada7bc8207f17efd0e8410b4fdf7d7e2
Julien Mottet <mottet.julien123@gmail.com>
2019-05-20 15:09:38 +0200

    devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show,:index]
+  skip_before_action :authenticate_user!, only: [:show]
 
   def new
     @event=Event.new
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home,:ressources, :news]
 
   def home
     @events = Event.all
