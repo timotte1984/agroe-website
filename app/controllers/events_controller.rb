ddfd312c41b3a0df7bfff16025db540068279321
Julien Mottet <mottet.julien123@gmail.com>
2019-05-22 22:39:34 +0200

    add widget and change adhesion cards

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :authenticate_user!, only: [:show,:index]
 
   def new
     @event=Event.new
