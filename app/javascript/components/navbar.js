75e454f948eb06a50233a27ae748a0a345a6bd5c
Julien Mottet <mottet.julien123@gmail.com>
2019-06-23 20:56:44 +0200

    delete banner

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar_img = document.querySelector('.navbar-wagon-brand');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 100) {
         navbar.classList.add('navbar-wagon-scroll');
         navbar_img.classList.add('navbar-wagon-brand-scroll');
       } else {
